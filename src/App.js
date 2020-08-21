88e6c55bbbf325c6fc992cf8772791a654382912
VickeTrossing <vitro001@gmail.com>
2020-08-21 13:01:30 +0200

    Tryning to add a function so you can add a task to the list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 
 class App extends Component {
@@ -12,12 +13,12 @@ class App extends Component {
 
   deleteTodo = (id) => {
     console.log(id);
-    // const todos = this.state.todos.filter(todo => {
-    //   return todo.id !== id
-    // });
-    // this.setState({
-    //   todos
-    // })
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
   }
 
   render(){
@@ -25,6 +26,7 @@ class App extends Component {
     <div className="todo-app container">
       <h1 className="center blue-text">Todos</h1>
       <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      <AddTodo/>
     </div>
   );
 }
